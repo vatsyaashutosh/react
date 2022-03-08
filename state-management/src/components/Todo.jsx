79d2c828632ace2b2712dc0ab279ceec18a14ea4
vatsyaashutosh <96139619+vatsyaashutosh@users.noreply.github.com>
2022-03-08 20:07:56 +0530

    done

--- state-management/src/components/Todo.jsx
+++ state-management/src/components/Todo.jsx
@@ -1,23 +1,35 @@
 import { useState } from "react";
 import { TodoInput} from "./TodoInput";
 import { TodoItem } from "./TodoItem";
+import { nanoid } from "nanoid";
 export const Todo = () =>{
     const [todos, setTodos] = useState([])
     const addTodo = (data) =>{
         const t={
-            id:1,
+            id:nanoid(),
             title:data,
             status:false
         }
-        setTodos([...todos,data])
+        setTodos([...todos,t])
         console.log([...todos,t])
+    }
+    const changeStatus = (id)=>{
+        let mapped=todos.map(e=>{
+            return e.id==id?{...e,status:!e.status}:{...e}
+        });
+        setTodos(mapped)
+    }
+    const deleteElement=(id)=>{
+        setTodos(todos.filter(to=>{
+           return (to.id!==id) 
+        }))
     }
   return (
     <div>
         <TodoInput addTodo={addTodo}/>
         {
             todos.map((e)=>(
-                <TodoItem todo={e}/>
+                <TodoItem changeStatus={changeStatus} deleteElement={deleteElement} todo={e} key={e.id}/>
             ))
 }
     </div>
--- state-management/src/components/TodoItem.jsx
+++ state-management/src/components/TodoItem.jsx
@@ -1,3 +1,13 @@
-export const TodoItem = ({todo}) =>{
-    return <div>{todo}</div>
+import './todo.css'
+export const TodoItem = ({todo,deleteElement,changeStatus}) =>{
+    return (
+
+    <div className={todo.status?"strike":""}>{todo.title} <button onClick={()=>{
+        changeStatus(todo.id)
+    }}>Toggle Todo</button>
+    <button onClick={()=>{
+        deleteElement(todo.id)
+    }}>Delete</button>
+    </div>
+    )
 }
